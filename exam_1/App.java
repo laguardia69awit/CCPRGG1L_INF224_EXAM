c482a8e8347eb39370d7381393af67f8d55be680
laguardia69awit <111828535+laguardia69awit@users.noreply.github.com>
2022-09-06 09:11:47 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) {
+
+        //Create a Student Class
+
+        //object - stu
+        //surname - Laguardia
+        //firstname - Luis Gabriel
+        //dateOfBirth - 05/04/04
+        //studentNumber - 2022104583
+        //studentEmailAddress - laguardialh@students.national-u.edu.ph
+        //iAmAwesome - true
+
+        Student stu = new Student();
+
+        stu.surname = "Laguardia";
+        stu.firstname = "Luis Gabriel";
+        stu.dateOfBirth = "05/04/04";
+        stu.studentNumber = 2022104583;
+        stu.studentEmailAddress = "laguardialh@students.national-u.edu.ph";
+        stu.iAmAwesome = true;
+
+        stu.sayMystudentNumber();
+        stu.sayMystudentEmailAddress();
+        stu.sayiAmAwesome();
+
+    }
+}
