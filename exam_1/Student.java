2b019ba15712342d903949d12144c8da6d2b21fa
laguardia69awit <111828535+laguardia69awit@users.noreply.github.com>
2022-09-06 09:12:57 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    
+String surname;
+String firstname;
+String dateOfBirth;
+int studentNumber;
+String studentEmailAddress;
+boolean iAmAwesome;
+
+    public void sayMystudentNumber(){
+        System.out.println(studentNumber);
+    }
+
+    public void sayMystudentEmailAddress(){
+        System.out.println(studentEmailAddress);
+    }
+
+    public void sayiAmAwesome(){
+        System.out.println(iAmAwesome);
+    }
+    
+}
